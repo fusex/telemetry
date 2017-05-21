30cd243cf898cd6775088f787a4837bbe60e65e1
Zakaria ElQotbi <zakaria@elqotbi.com>
2017-05-21 12:50:02 +0200

    integrate imu code

---
+++ rocket/src/imu.h
@@ -0,0 +1,20 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  imu.h
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  20/05/2017 17:31:08
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Zakaria ElQotbi (zskdan), zakaria@derbsellicon.com
+ *        Company:  Derb.io 
+ *
+ * =====================================================================================
+ */
+
+void setupImu();
+void loopImu();
--- rocket/src/sketch.ino
+++ rocket/src/sketch.ino
@@ -1,14 +1,17 @@
 #include "common.h"
+#include "imu.h"
 #include "radio.h"
 
 void setup()
 {
   setupCommon();
+  setupImu();
   setupRadio();
 }
 
 void loop()
 {
+  loopImu();
   loopRadio();
 }
 
