8bd29dfa30fe34f22628119b0649d4b75af3dfed
Zakaria ElQotbi <zakaria@elqotbi.com>
2017-05-25 17:29:04 +0200

    integrate first version of propellant detector

--- rocket/src/propellant.cpp
+++ rocket/src/propellant.cpp
@@ -16,11 +16,18 @@
  * =====================================================================================
  */
 
+#include <Arduino.h>
+#include <fusexutil.h>
+#include <fusexconfig.h>
+
+const int thresholdvalue=490; //The threshold
+
 void setupPropellant()
 {
 #ifndef CONFIG_PROPELLANT
     return;
 #endif
+   TRACE("Init Propellant sound detector\n\r");
 }
 
 void loopPropellant()
@@ -28,5 +35,11 @@ void loopPropellant()
 #ifndef CONFIG_PROPELLANT
     return;
 #endif
+    int sensorValue = analogRead(A13); //use A0 to read the electrical signal
+    if(sensorValue>thresholdvalue)
+    {
+	TRACE("Propellant detect:%d\r\n", sensorValue);
+    }
+    delay(200);
 }
 
