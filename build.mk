49f2f605b08b0d7d4d9f3cb73b82cf4880fc7395
Zakaria ElQotbi <zakaria@elqotbi.com>
2017-05-25 20:40:31 +0200

    change build system to arduino official
    Starting from Arduino 1.5, compilation can be done from
    command line (cli).
    thus here we drop the inotool support and rewrite the buildsystem
    to rebased on officiel arduino tools.
    
    Signed-off-by: Zakaria ElQotbi <zakaria@elqotbi.com>

---
+++ build.mk
@@ -0,0 +1,27 @@
+ARDUINO_PATH := /opt/arduino-1.8.1
+BUILD_PATH   := .build
+ENTRY_POINY  := src/sketch.ino
+TERMINAL     := picocom
+
+serial-port ?= /dev/ttyACM0
+board-model ?= uno
+
+ARDUINO_CMD := $(ARDUINO_PATH)/arduino --port $(serial-port) --board arduino:avr:$(board-model) --pref build.path=$(BUILD_PATH) --pref sketchbook.path=.  
+
+.DEFAULT_GOAL := compile
+
+.PHONY: compile upload serial clean
+
+build: compile
+
+compile:
+	$(ARDUINO_CMD) --verify $(ENTRY_POINY) 
+
+upload:
+	$(ARDUINO_CMD) --upload $(ENTRY_POINY) 
+
+serial:
+	$(TERMINAL) $(serial-port)
+
+clean:
+	$(RM) -r $(BUILD_PATH)
