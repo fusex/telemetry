1c1c006a99203be177fc19c62d1989b2ced87840
Zakaria ElQotbi <zakaria@elqotbi.com>
2017-07-03 01:10:55 +0200

    add pinout

---
+++ rocket/src/pinout.h
@@ -0,0 +1,26 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  pinout.h
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  30/06/2017 01:17:23
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Zakaria ElQotbi (zskdan), zakaria@derbsellicon.com
+ *        Company:  Derb.io 
+ *
+ * =====================================================================================
+ */
+
+#ifndef _PINOUT_H
+#define _PINOUT_H
+
+#define SOUND_SENSOR A13
+#define IMU_INTR     12
+
+#endif //_PINOUT_H
+
