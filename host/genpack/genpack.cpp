9dab8e3fae23705ee77e167284971c00807b542e
Zakaria ElQotbi <zakaria@elqotbi.com>
2018-06-03 02:52:02 +0200

    adding an utility that generate samples

---
+++ host/genpack/genpack.cpp
@@ -0,0 +1,109 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  fxhost.c
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  07/06/2017 00:34:59
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+*         Author:  Zakaria ElQotbi (zakariae), zakaria.elqotbi@derbsellicon.com
+*        Company:  Derb.io 
+ *
+ * =====================================================================================
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+
+#include <string.h>
+#include <termios.h>
+#include <unistd.h>
+#include <errno.h>
+#include <fcntl.h> 
+#include <signal.h>
+#include <time.h>
+
+#include "fusexutil.h"
+#include "trame.h"
+#include "logger.h"
+
+#if 1
+# define HEX_DUMP   0
+# define TRAME_DUMP 1
+#elif 0
+# define HEX_DUMP   1
+# define TRAME_DUMP 0
+#else
+# define HEX_DUMP   0
+# define TRAME_DUMP 0
+#endif
+
+
+void do_usage(char** argv)
+{
+    printf ("\tUsage: %s [-h] outputfile [NR] \n",argv[0]);
+    exit(0);
+}
+
+int main(int argc, char** argv)
+{
+    FILE*          file = NULL;
+    int32_t        nr_packets = 1;
+    uint32_t       wb = 0;
+    const uint32_t bs = fxtm_getblocksize();
+
+    if(argc < 2) {
+	printf("please provide file\n");
+	exit(1);
+    }
+    if(!strncmp("-h",argv[1],2))
+ 	do_usage(argv);	
+    if(argc > 1)
+	file = fopen(argv[1],"wo"); 
+    if(argc>2)
+    	nr_packets = strtol(argv[2],NULL,10);
+
+    if(!file) {
+        printf("Error opening %s: %s\n", argv[1], strerror(errno));
+	exit(-1);
+    }
+
+    for(int32_t i=0; i<nr_packets; i++){
+	uint8_t*     buf = (uint8_t*) fxtm_getblock();
+	fxtm_data_t* tm  = fxtm_getdata(); 
+
+        memset(buf,0,bs);
+	
+        float x = (float)((i*10)+i)/10;
+	float y = (float)((i*10)-i)/10;
+	float z = (float)i;
+
+	float accel[3] = {x,y,z};
+	float magn[3]  = {y,x,z};
+	float gyr[3]   = {z,y,x};
+	fxtm_setimu(accel, magn, gyr); 
+
+	tm->timestamp = _mymillis(); 
+	tm->id = i;
+
+	fxtm_setsoundlvl((i%MAX_SOUND_LEVEL));
+	fxtm_settemperature((i%MAX_TEMPERATURE));
+	fxtm_setpressure((MAX_PRESSURE_AT_SEALEVEL - i));
+	fxtm_setgps(x, y);
+	fxtm_dump(NULL);
+
+	if(fwrite(buf, bs, 1, file) == 0){
+	    printf("Error writing in %s %s\n", argv[1], strerror(errno));
+	    break;
+	}
+	wb += bs;
+	usleep(1000); // a tempo to make the timestamp progress
+    }
+    printf("%d bytes writed\n", wb);
+
+    fclose(file);
+}
