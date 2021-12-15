128b0ba8cb931f9a25b6a9f8c953d0a1795b2366
Zakaria ElQotbi <zakaria@elqotbi.com>
2021-12-15 14:33:59 +0100

    move profiling code to separate module

--- rocket/src/sketch.ino
+++ rocket/src/sketch.ino
@@ -5,20 +5,11 @@
 #include "sdcard.h"
 #include "propellant.h"
 #include "rtc.h"
+#include "prof.h"
 
 #include <fusexutil.h>
 #include "trame.h"
 #include "sdcard.h"
-
-static bool once = true;
-
-#ifdef PROFILING
-static uint32_t max = 0;
-static uint32_t avg = 0;
-static uint32_t count = 0;
-#define LOOP 200000UL
-//#define LOOP 1000
-#endif
 
 static void acquire()
 {
@@ -54,32 +45,16 @@ void setup()
     setupPropellant();
     setupSdcard();
     setupFinishCommon();
-}
 
+    TTRACE("#########################\n\r");
+	TTRACE("Start transfer packet size:%d\r\n",fxtm_getdatasize());
+}
 
 void loop()
 {
-    if(once){
-	TTRACE("#########################\n\r");
-	TTRACE("Start transfer packet size:%d\r\n",fxtm_getdatasize());
-        once = false;
-    }
-
-#ifdef PROFILING
-    uint32_t time = micros();
-#endif
-
+    prof_start();
     acquire();
     log();
     send();
-
-#ifdef PROFILING 
-    uint32_t d1 = micros() - time;
-    if(d1>max) max = d1; 
-    avg += d1;
-
-    if(count++>LOOP) {
-	TTRACE("%ld loop in:%lu (avg:%lu, max:%lu)\r\n", count, d1 ,avg/(count+1), max);
-    }
-#endif
+    prof_report();
 }
