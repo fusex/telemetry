90511f2607a99c8c1f841eb16ee4ef2fe004d31a
Zakaria ElQotbi <zakaria@elqotbi.com>
2022-01-16 01:33:26 +0100

    remove bmp code from imu

--- rocket/src/imu.cpp
+++ rocket/src/imu.cpp
@@ -21,35 +21,24 @@
 #include <fusexconfig.h>
 #include <fusexutil.h>
 
-#include "MPU9250.h"
-#include <Adafruit_BMP280.h>
 #include "Wire.h"
+#include "MPU9250.h"
 
 #include "BGC_Pinout.h"
 #include "trame.h"
 
-Adafruit_BMP280 bmp;
 MPU9250 myIMU;
-float   temperature;
-float   pressure;
 
 #if ADO
 #define MPU_AM_I_RET 0x71
 #else
 #define MPU_AM_I_RET 0x73
 #endif
+#define AK_AM_I_RET 0x48
 
 #if 0
 #define IMU_DEBUG 1 
 #endif
-#define AK_AM_I_RET 0x48
-
-static int initBMP()
-{
-    if (!bmp.begin(0x76,0x58)) {  
-	return -3;
-    }
-}
 
 static int initMPU()
 {
@@ -96,22 +85,19 @@ static int initAK()
 void setupIMU()
 {
     Wire.begin();
-    if(!initMPU()){
-	TTRACE("init Failed! fatal !!!\r\n");
-	Init_SetSemiFatal();
-	return;
+    if (!initMPU())
+    {
+        TTRACE("init Failed! fatal !!!\r\n");
+        Init_SetSemiFatal();
+        return;
     }
-    if(!initAK()){
-	TTRACE("init Failed! fatal !!!\r\n");
-	Init_SetFailed();
-	return;
-    }
-    if(!initBMP()){
-	Init_SetFailed();
-	return;
+    if (!initAK())
+    {
+        TTRACE("init Failed! fatal !!!\r\n");
+        Init_SetFailed();
+        return;
     }
 
-    delay(2000);
     TTRACE("init Done.\r\n");
 }
 
@@ -119,7 +105,6 @@ int intrIMU()
 {
     return (myIMU.readByte(MPU9250_ADDRESS, INT_STATUS) & 0x01);
 }
-
 
 void loopIMU()
 {
@@ -152,48 +137,11 @@ void loopIMU()
 
     myIMU.temperature = ((float) myIMU.tempCount) / 333.87 + 21.0;
 
-#if 1
-    temperature = bmp.readTemperature();
-#else
-    temperature = myIMU.temperature();
-#endif
-    pressure = bmp.readPressure();
-
-#if IMU_DEBUG
-	PRINT("Temperature: "); PRINTLN(temperature,2);
-	PRINT("Pressure: "); PRINTLN(pressure,2);
-	PRINT("Altitude: "); PRINTLN(bmp.readAltitude(1024.3));
-
-	PRINT("X-acceleration: "); PRINT(myIMU.ax);
-	PRINT(" mg ");
-	PRINT("Y-acceleration: "); PRINT(myIMU.ay);
-	PRINT(" mg ");
-	PRINT("Z-acceleration: "); PRINT(myIMU.az);
-	PRINTLN(" mg ");
-
-	PRINT("X-gyro rate: "); PRINT(myIMU.gx, 3);
-	PRINT(" degrees/sec ");
-	PRINT("Y-gyro rate: "); PRINT(myIMU.gy, 3);
-	PRINT(" degrees/sec ");
-	PRINT("Z-gyro rate: "); PRINT(myIMU.gz, 3);
-	PRINTLN(" degrees/sec");
-
-	PRINT("X-mag field: "); PRINT(myIMU.mx/1000);
-	PRINT(" mG ");
-	PRINT("Y-mag field: "); PRINT(myIMU.my/1000);
-	PRINT(" mG ");
-	PRINT("Z-mag field: "); PRINT(myIMU.mz/1000);
-	PRINTLN(" mG");
-#endif
-
     float a[] = {myIMU.ax, myIMU.ay, myIMU.az};
     float m[] = {myIMU.mx/1000, myIMU.my/1000, myIMU.mz/1000}; 
     float g[] = {myIMU.gx, myIMU.gy, myIMU.gz};
 
     fxtm_setimu(a, m, g);
-    fxtm_settemperature(temperature);
-    fxtm_setpressure(pressure);
-
     DTRACE("ZSK packet acquired in:%ld and prepared in %ld us\r\n", d1, micros()-time);
 
     myIMU.updateTime();
