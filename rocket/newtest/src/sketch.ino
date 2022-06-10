fe003039674c423b35d8912564a08582e10f0ca8
Zakaria ElQotbi <zakaria@elqotbi.com>
2022-06-10 11:52:47 +0200

    remove the test code

--- rocket/newtest/src/sketch.ino
+++ rocket/newtest/src/sketch.ino
@@ -1,30 +1,6 @@
-#include <SPI.h>
-#include <BGC_Lora.h>
 
-#if 1
-BGC_Lora lora;           // BGC Sens board.
-#else
-BGC_Lora lora(10, true); //Lora shield on DUE.
-#endif
-
-void setup()
-{
-  // put your setup code here, to run once:
-  Serial.begin(115200);
-  if (!lora.init()) {
-    Serial.println("init failed");
-    while(1) {
-      delay(100000);
-    }
-  }
-  Serial.println("Lora Init OK");
+void setup() {
 }
 
-void loop()
-{
-  uint8_t data[] = "Hello World!";
-  Serial.println("Sending on BGC Lora");
-  lora.send(data, sizeof(data));
-           
-  delay(400);
+void loop() {
 }
