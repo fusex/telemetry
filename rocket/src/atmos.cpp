8d6ca3b016f65da3eace06c59e555be97dfc209b
Zakaria ElQotbi <zakaria@elqotbi.com>
2022-08-03 00:26:27 +0200

    remove delays in i2c transaction of hdc1080 sensor

--- rocket/src/atmos.cpp
+++ rocket/src/atmos.cpp
@@ -25,7 +25,7 @@
 #include <Wire.h>
 #include <Adafruit_BMP280.h>
 #include <ClosedCube_HDC1080.h>
-#include <BGC_Pinout.h>
+#include <BGC_I2c.h>
 
 #include "trame.h"
 
@@ -34,12 +34,12 @@ static Adafruit_BMP280 bmp;
 
 void setupAtmos()
 {
-    if (!bmp.begin(0x76, 0x58)) {
+    if (!bmp.begin(BGC_I2C_PRESSURE_ADDR, 0x58)) {
         Init_SetFailed();
         return;
     }
 
-   hdc1080.begin(0x40);
+   hdc1080.begin(BGC_I2C_TEMP_HUM_ADDR);
    if (hdc1080.readManufacturerId() != 0x5449 ||
            hdc1080.readDeviceId() != 0x1050) {
         Init_SetFailed();
