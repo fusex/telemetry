333b17bd04d7ce600a34027b020c1fea7b6e18f4
Zakaria ElQotbi <zakaria@elqotbi.com>
2022-08-23 11:24:12 +0200

    switch the imu code to sparkfun library

--- rocket/src/imu.cpp
+++ rocket/src/imu.cpp
@@ -6,45 +6,49 @@
 #include <BGC_Pinout.h>
 #include <BGC_I2c.h>
 #include <trame.h>
-#include <ICM20948_WE.h>
-#include "ICM20600.h"
+
+#include <ICM_20948.h>
+#include <ICM20600.h>
 
 #include "init.h"
 
 #define isAD0(x) (x&1)
-ICM20948_WE myImu = ICM20948_WE(BGC_I2C_MAIN_IMU_ADDR);
-ICM20600    myImuAux = ICM20600(isAD0(BGC_I2C_AUX_IMU_ADDR));
 
-#if 1
-#define IMU_CALIBRATION
-#endif
+ICM_20948_I2C myImu;
+ICM20600      myImuAux = ICM20600(isAD0(BGC_I2C_AUX_IMU_ADDR));
 
 static int initICM20948 ()
 {
-    if (myImu.init() == false)
+    myImu.begin(Wire, isAD0(BGC_I2C_MAIN_IMU_ADDR));
+    if (myImu.status != ICM_20948_Stat_Ok)
         return false;
 
-#ifdef IMU_CALIBRATION
-    myImu.autoOffsets();
+    /* Reset the IMU in a known state */
+    myImu.swReset();
+    myImu.sleep(false); 
+    myImu.lowPower(false); 
+
+    /* Set the scale to 16G for accel and 500 for Gyro */
+    ICM_20948_fss_t myFSS =
+    	{ .a = gpm16, .g = dps500 };
+    myImu.setFullScale(ICM_20948_Internal_Acc|ICM_20948_Internal_Gyr, myFSS);
+
+    /* Set the sampling rate to 10 Hz */
+    myImu.setSampleMode(
+	(ICM_20948_Internal_Acc | ICM_20948_Internal_Gyr),
+	ICM_20948_Sample_Mode_Cycled);
+    ICM_20948_smplrt_t mySmplrt = {.a = 10, .g = 10};
+    myImu.setSampleRate(ICM_20948_Internal_Acc|ICM_20948_Internal_Gyr, mySmplrt);
+
+#if 0
+    /* Enable a Filter */
+    ICM_20948_dlpcfg_t myDLPcfg = 
+    	{.a = acc_d11bw5_n17bw, .g = gyr_d11bw6_n17bw8};
+    myImu.setDLPFcfg(ICM_20948_Internal_Acc|ICM_20948_Internal_Gyr, myDLPcfg);
 #endif
 
-    myImu.setGyrRange(ICM20948_GYRO_RANGE_250);
-    myImu.setGyrDLPF(ICM20948_DLPF_6);  
-    myImu.setGyrSampleRateDivider(10);
-
-    myImu.setAccRange(ICM20948_ACC_RANGE_16G);
-    myImu.setAccDLPF(ICM20948_DLPF_6);    
-    myImu.setAccSampleRateDivider(10);
-
-    return true;
-}
-
-static int initAK ()
-{
-    if (!myImu.initMagnetometer())
+    if (myImu.status != ICM_20948_Stat_Ok)
         return false;
-
-    myImu.setMagOpMode(AK09916_CONT_MODE_20HZ);
 
     return true;
 }
@@ -66,17 +70,9 @@ void setupIMU ()
 
     if (initICM20948() == false)
     {
-        TTRACE("init ICM20948 Failed! fatal !!!\r\n");
+        CTRACE("init ICM20948 Failed! fatal !!!\r\n");
         failed = true;
     }
-
-#if 1
-    if (initAK() == false)
-    {
-        TTRACE("init ICM20948-AK Failed! fatal !!!\r\n");
-        failed = true;
-    }
-#endif
 
     if (initICM20600() == false) {
         CTRACE("init ICM20600 Failed! fatal !!!\r\n");
@@ -84,7 +80,7 @@ void setupIMU ()
     }
 
     if (failed == false) {
-        TTRACE("init Done.\r\n");
+        CTRACE("init Done.\r\n");
         module_setup(TAG, FXTM_SUCCESS);
     } else {
         module_setup(TAG, FXTM_FAILURE);
@@ -94,26 +90,21 @@ void setupIMU ()
 
 void loopIMU ()
 {
-    myImu.readSensor();
-#if 0
-    xyzFloat acc = myImu.getAccRawValues();
-    xyzFloat gyr = myImu.getGyrValues();
-#else
-    xyzFloat acc = myImu.getCorrectedAccRawValues();
-    xyzFloat gyr = myImu.getCorrectedGyrRawValues();
-#endif
-    xyzFloat mag = myImu.getMagValues();
+    if (myImu.dataReady()) {
+	myImu.getAGMT();
 
-    float a[] = {acc.x, acc.y, acc.z};
-    float m[] = {mag.x, mag.y, mag.z}; 
-    //float m[] = {mag.x/1000, mag.y/1000, mag.z/1000}; 
-    float g[] = {gyr.x, gyr.y, gyr.z};
+	float a[] = {myImu.accX(), myImu.accY(), myImu.accZ()};
+	float m[] = {myImu.magX(), myImu.magY(), myImu.magZ()};
+	float g[] = {myImu.gyrX(), myImu.gyrY(), myImu.gyrZ()};
+
+	fxtm_setimu(a, m, g);
+    }
+
     float a2[] = {
-        myImuAux.getAccelerationX(),
-        myImuAux.getAccelerationY(),
-        myImuAux.getAccelerationZ()
+	myImuAux.getAccelerationX(),
+	myImuAux.getAccelerationY(),
+	myImuAux.getAccelerationZ()
     };
 
-    fxtm_setimu(a, m, g);
     fxtm_setimu2(a2);
 }
