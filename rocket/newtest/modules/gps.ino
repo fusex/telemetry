610140b691101deda0e6881ba703007a6d552883
Zakaria ElQotbi <zakaria@elqotbi.com>
2023-06-24 10:05:06 +0200

    make gps test to user Serial as debug trace

--- rocket/newtest/modules/gps.ino
+++ rocket/newtest/modules/gps.ino
@@ -1,14 +1,14 @@
 void setup()
 {
-    Serial2.begin(115200);
-    while (!Serial2) ; // Wait for DEBUG serial port to be available
-    Serial1.begin(9600);
+    Serial.begin(115200);
+    while (!Serial) ; // Wait for DEBUG serial port to be available
 
+    Serial1.begin(9600);
     while (!Serial1) {
-        Serial2.println("init failed ! Retrying !");
+        Serial.println("init failed ! Retrying !");
         delay(2000);
     }
-    Serial2.println("Rocket GPS test ");
+    Serial.println("Rocket GPS test ");
 }
 
 void loop()
@@ -18,7 +18,7 @@ void loop()
 
     while (f == 0) {
         while (Serial1.available()) {
-            Serial2.write(Serial1.read());
+            Serial.write(Serial1.read());
             //if (++c>1000) f = 1;
         }
     }
